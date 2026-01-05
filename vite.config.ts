ca3fa5e10583f5ac6f1580ed423d27936a9decef
theunis247 <local360solutions@gmail.com>
2026-01-05 21:11:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+    preview: {
+      port: 25578,
+      host: '0.0.0.0'
+    },
       plugins: [react()],
       define: {
         'process.env.DEEPSEEK_API_KEY': JSON.stringify(env.DEEPSEEK_API_KEY),
